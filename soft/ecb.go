665db24e58a7e68c7198652a48bf42abcf751411
Jonathan Lima <greenboxal@gmail.com>
2016-08-25 20:36:28 -0300

    Add ECB implementation

---
+++ soft/ecb.go
@@ -0,0 +1,26 @@
+package soft
+
+import (
+	"crypto/cipher"
+)
+
+type ecbBlockMode struct {
+	impl cipher.Block
+	encrypt bool
+}
+
+func (e *ecbBlockMode) BlockSize() int {
+	return e.impl.BlockSize()
+}
+
+func (e *ecbBlockMode) CryptBlocks(dst, src []byte) {
+	bs := e.BlockSize()
+
+	for i := 0; i < len(src); i += bs {
+		if e.encrypt {
+			e.impl.Encrypt(dst[i:i+bs], src[i:i+bs])
+		} else {
+			e.impl.Decrypt(dst[i:i+bs], src[i:i+bs])
+		}
+	}
+}
